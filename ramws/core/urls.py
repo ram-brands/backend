fed55a4c078dc41e53363bcf5e1e478effadb3ce
Ariel Martínez <ariel@martinezs.dev>
2021-09-15 15:04:23 +0200

    Fix url ordering

--- ramws/core/urls.py
+++ ramws/core/urls.py
@@ -3,10 +3,10 @@ from django.urls import include, path
 from .views import admin_site, health_check, trigger_error
 
 urlpatterns = [
-    path("", admin_site.urls, name="admin"),
     path("error", trigger_error, name="error"),
     path("health", health_check, name="health"),
     path("", include("accounts.urls")),
     path("", include("docs.urls")),
     path("", include("files.urls")),
+    path("", admin_site.urls, name="admin"),
 ]
